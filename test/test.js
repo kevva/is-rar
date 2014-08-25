bae5431178b952f34bb45c616b5f2a46215dd822
Kevin Martensson <kevinmartensson@gmail.com>
2014-08-25 02:11:48 +0200

    Use ava for testing

--- test/test.js
+++ test/test.js
@@ -1,23 +1,15 @@
-/*global describe, it */
 'use strict';
 
-var assert = require('assert');
-var fs = require('fs');
 var isRar = require('../');
 var path = require('path');
+var readChunk = require('read-chunk');
+var test = require('ava');
 
-describe('isRar()', function () {
-    function check(file) {
-        return isRar(fs.readFileSync(file));
-    }
+test('should detect RAR from buffer', function (t) {
+    t.plan(2);
 
-    it('should detect RAR from buffer', function (cb) {
-        assert(check(path.join(__dirname, 'fixtures/test.rar')));
-        cb();
-    });
-
-    it('should detect RAR from Uint8Array', function () {
-        var buf = new Uint8Array([82, 97, 114, 33, 26, 7, 1]);
-        assert(isRar(buf));
+    readChunk(path.join(__dirname, 'fixtures/test.rar'), 0, 8, function (err, buf) {
+        t.assert(!err);
+        t.assert(isRar(buf));
     });
 });
