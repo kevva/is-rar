7fe2b6992c5a09ed800b1719647f8aad6e2e4529
Kevin Martensson <kevinmartensson@gmail.com>
2015-01-28 17:08:00 +0100

    Various tweaks

---
+++ readme.md
@@ -0,0 +1,23 @@
+# is-rar [![Build Status](http://img.shields.io/travis/kevva/is-rar/master.svg?style=flat))](https://travis-ci.org/kevva/is-rar)
+
+> Check if a Buffer/Uint8Array is a RAR file
+
+## Install
+
+```sh
+$ npm install --save is-rar
+```
+
+## Usage
+
+```js
+var isRar = require('is-rar');
+var read = require('fs').readFileSync;
+
+isRar(read('foo.rar'));
+//=> true
+```
+
+## License
+
+MIT © [Kevin Mårtensson](https://github.com/kevva)
--- test/test.js
+++ test/test.js
@@ -6,10 +6,10 @@ var readChunk = require('read-chunk');
 var test = require('ava');
 
 test('should detect RAR from buffer', function (t) {
-    t.plan(2);
+	t.plan(2);
 
-    readChunk(path.join(__dirname, 'fixtures/test.rar'), 0, 8, function (err, buf) {
-        t.assert(!err);
-        t.assert(isRar(buf));
-    });
+	readChunk(path.join(__dirname, 'fixtures/test.rar'), 0, 8, function (err, buf) {
+		t.assert(!err, err);
+		t.assert(isRar(buf));
+	});
 });
